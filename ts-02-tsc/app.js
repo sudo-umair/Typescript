a62fc6e0e7a4da74b4da27d72ec1e440f97673ae
Muhammad Umair <muhammadumair523@gmail.com>
2022-10-02 22:14:10 +0500

    Compiler Configuration

---
+++ ts-02-tsc/app.js
@@ -0,0 +1,4 @@
+"use strict";
+const userName = "Muhammad Umair";
+let age = 25;
+console.log(userName);
---
+++ ts-02-tsc/app.ts
@@ -0,0 +1,4 @@
+const userName: string = "Muhammad Umair";
+let age: number = 25;
+
+console.log(userName);
