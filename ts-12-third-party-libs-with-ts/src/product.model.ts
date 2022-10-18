d4a446038a19f1f59779b7a6885f4a96324553f9
Muhammad Umair <muhammadumair523@gmail.com>
2022-10-18 13:11:33 +0500

    module 12 completed

---
+++ ts-12-third-party-libs-with-ts/src/product.model.ts
@@ -0,0 +1,18 @@
+import { IsNumber, IsPositive, IsNotEmpty } from 'class-validator';
+
+export class Product {
+  @IsNotEmpty()
+  title: string;
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+
+  getInformation() {
+    return [this.title, `$${this.price}`];
+  }
+}
